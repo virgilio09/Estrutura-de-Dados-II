fb67c1b4ebf2bc0de3562b33b34ff5ca31da7273
João Virgilio <jvirgiliomartins09@gmail.com>
2020-12-11 19:52:20 -0300

    Estrutura da arvore, realizada

--- Trabalho-01/quest03.c
+++ Trabalho-01/quest03.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct lista{
 	int info;
@@ -12,6 +13,80 @@ typedef struct arv{
 	struct arv *esq, *dir;
 }No;
 
+Lista *insere_lst(Lista *l, int i){
+	Lista* novo = (Lista*) malloc(sizeof(Lista));
+	novo->info = i;
+	novo->prox = l;
+	return novo;
+}
+
+
+No *aloca_arv(char *palavra){
+
+    No *nova;
+
+    nova = (No*)malloc(sizeof(No));
+
+    strncpy(nova->palavra, palavra, strlen(palavra));    
+    nova->linha = NULL;
+    nova->esq = NULL;
+    nova->dir = NULL;
+
+    return nova;
+
+}
+
+
+void insere_arv(No **raiz, No *a){
+
+    if(*raiz == NULL)
+        *raiz = a;
+
+    else{
+    	if(strcmp(a->palavra, (*raiz)->palavra) < 0)
+            insere_arv(&(*raiz)->esq, a);
+
+        else if(strcmp(a->palavra, (*raiz)->palavra) > 0)
+            insere_arv(&(*raiz)->dir, a);
+    } 
+       
+}
 
 
 
+void imprime_arv(No *raiz){
+	if(raiz != NULL){
+		imprime_arv(raiz->esq);
+		printf("%s\n",raiz->palavra);
+		imprime_arv(raiz->dir);
+	}
+}
+
+void imprime_lst(Lista* l){
+	
+	Lista* p;
+	p = l;
+	if(p != NULL){
+		printf("info = %d\n", p->info);
+		imprime_lst(p->prox);
+
+	}
+}
+
+int main(){
+	No *raiz, *a;
+	raiz = NULL;
+	
+	a = aloca_arv("joaoo");
+	insere_arv(&raiz, a);
+
+	a = aloca_arv("Ana");
+	insere_arv(&raiz, a);
+
+	imprime_arv(raiz);
+
+
+
+
+	return 0;
+}
